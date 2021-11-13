2c0d74bb11b8fba07b7cc9cf0b34291673fb65ef
effortguy <xlcuqhdl@gmail.com>
2021-11-13 16:01:12 +0900

    feat: ThreadLocalTraceLog 추가

---
+++ src/main/java/hello/advanced/app/v2/OrderServiceV2.java
@@ -0,0 +1,30 @@
+package hello.advanced.app.v2;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepositoryV2;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(String itemId, TraceId traceId) {
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.beginSync(traceId, "OrderServiceV1.request()");
+            orderRepositoryV2.save(itemId, status.getTraceId());
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
